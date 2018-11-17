eef4dc7398e8bffe1c18738443f52d18210ae5ff
v3x4t0r <vexator@agent-orange>
2018-11-17 22:05:30 +0000

    web-test1

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello World!"
+    return "Hello Henrik!"
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
