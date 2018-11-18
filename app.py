3c188be81aeee5a5f7f23d6756581ef26004cd91
v3x4t0r <33897075+v3x4t0r@users.noreply.github.com>
2018-11-18 11:45:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello Henrik!"
+    return "Hello Nordea!"
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
